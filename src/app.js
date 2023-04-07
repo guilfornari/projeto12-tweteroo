03ad85ad4d1607798962a46253f6d87f5bed5d53
Guilfornari <guilfornari@gmail.com>
2023-04-07 16:05:15 -0300

    Testing requisite signup

--- src/app.js
+++ src/app.js
@@ -3,15 +3,27 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-app.get("/test", (req, res) => {
+const usersList = [];
+const usersTweets = [];
+
+app.post("/signup", (req, res) => {
+    const { username, avatar } = req.body;
+    const newUser = { username, avatar };
+    usersList.push(newUser);
+    res.send("OK");
+});
+
+app.get("/", (req, res) => {
     const tryOut = { trial: "first", time: 1 };
     res.send(tryOut);
-})
+});
 
 app.get("/lista-testes", (req, res) => {
     const trials = [{ trial: "first", time: 1 }, { trial: "Second", idade: 2 }];
     res.send(trials);
-})
+});
 
-app.listen(5000, () => console.log("Running server on port 5000"));
+app.listen(5000, () => console.log("Server is running on port 5000, berk!"));
+
